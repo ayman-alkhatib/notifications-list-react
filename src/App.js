5af32e538a726711c4b19d09eaf89986792f1b3e
Ayman ALKHATIB <aemn22384@gmail.com>
2024-12-04 21:58:34 +0100

    use Notification component in main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Notification from "./components/Notification";
+import { notificationData } from "./notificationsData";
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {notificationData.map(notificationObject =>
+        (<Notification
+          notificationObject={notificationObject}
+          key={notificationObject.id} />
+        ))}
 
       </header>
     </div>
