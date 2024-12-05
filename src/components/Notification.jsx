ff63ec3be933e53602c6aceddca626355cad7f72
Ayman ALKHATIB <aemn22384@gmail.com>
2024-12-05 09:48:15 +0100

    modify the layout of the notification component from grid to flexbox

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -20,24 +20,26 @@ function Notification({ notificationObject }) {
     >
       <Avatar imgSrc={avatar} />
       <div className={styles.textBox}>
-        <span className={styles.username}>
-          <strong> {username} </strong>
-        </span>
-        <span className={styles.notificationType}> {notificationType} </span>
-        {readStatus && <span className={styles.activeNotification}></span>}
-        {target && (
-          <span
-            className={styles.target}
-            style={{
-              color: notificationType.toLowerCase().includes("group")
-                ? "hsl(219, 85%, 26%)"
-                : "",
-            }}
-          >
-            {" "}
-            {target}{" "}
+        <div className="notificationBody">
+          <span className={styles.username}>
+            <strong> {username} </strong>
           </span>
-        )}
+          <span className={styles.notificationType}> {notificationType} </span>
+          {readStatus && <span className={styles.activeNotification}></span>}
+          {target && (
+            <span
+              className={styles.target}
+              style={{
+                color: notificationType.toLowerCase().includes("group")
+                  ? "hsl(219, 85%, 26%)"
+                  : "hsl(219, 12%, 42%)",
+              }}
+            >
+              {" "}
+              {target}{" "}
+            </span>
+          )}
+        </div>
         <span className={styles.timestamp}> {timestamp} </span>
         {messagePreview && <p> {messagePreview} </p>}
       </div>
