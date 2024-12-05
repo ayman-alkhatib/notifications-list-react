252e7d5b683c8a05caa1917c4337affc1d2597c4
Ayman ALKHATIB <aemn22384@gmail.com>
2024-12-05 10:32:14 +0100

    add header and container to app component and the main layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import Notification from "./components/Notification";
 import { notificationData } from "./notificationsData";
-
+import styles from './App.module.css'
+import { useState } from "react";
 
 
 
 function App() {
+  const [notificationDataState, setNotificationDataState] = useState(notificationData)
+  function handleMarkAllASUnRead() {
+    setNotificationDataState((state) => {
+      return state.map((notificationObject) => { return { ...notificationObject, readStatus: false } })
+    })
+  }
+  const notificationActiveNum = notificationDataState.reduce((prev, cur) => {
+    if (cur.readStatus) return prev = prev + 1
+    return prev
+  }, 0)
   return (
-    <div className="App">
-      <header className="App-header">
-        {notificationData.map(notificationObject =>
-        (<Notification
-          notificationObject={notificationObject}
-          key={notificationObject.id} />
-        ))}
+    <div className={styles.app}>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <h1>Notifications <span>{notificationActiveNum}</span></h1>
 
-      </header>
+          <button onClick={handleMarkAllASUnRead}>Mark all as read</button>
+        </header>
+
+        <main>
+          {notificationDataState.map(notificationObject =>
+          (<Notification
+            notificationObject={notificationObject}
+            key={notificationObject.id} />
+          ))}
+        </main>
+      </div>
     </div>
   );
 }
