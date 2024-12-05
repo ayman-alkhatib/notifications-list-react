640f1c978302bc4dd53ace2ccea0673aace4e4b3
Ayman ALKHATIB <aemn22384@gmail.com>
2024-12-05 12:42:42 +0100

    create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+function Footer() {
+  return (
+    <footer className="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://www.linkedin.com/in/ayman---alkhatib"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Ayman Alkhatib
+      </a>
+      .
+    </footer>
+  );
+}
+
+export default Footer;
